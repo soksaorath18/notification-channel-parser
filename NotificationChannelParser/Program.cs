5e9ead79d7c300d39031d703d8b4bcb8de8c463b
Ramon <ramon.sok@pathmazing.com>
2023-09-29 21:40:14 +0700

    init project

---
+++ NotificationChannelParser/Program.cs
@@ -0,0 +1,31 @@
+﻿static void ParseNotificationChannel(string notification) {
+  string[] notificationChannels = {"BE", "FE", "QA", "Urgent"};
+  List<string> foundChannels = new();
+
+  string channel = "";
+  bool isOpenTag = false;
+  for (int i = 0; i < notification.Length; i++) {
+    char c = notification[i];
+
+    if (c == '[') {
+      isOpenTag = true;
+      continue;
+    } else if (c == ']') {
+      if (!string.IsNullOrEmpty(channel) && notificationChannels.Contains(channel)) {
+        foundChannels.Add(channel);
+      }
+      channel = "";
+      isOpenTag = false;
+      continue;
+    }
+
+    if (isOpenTag) {
+      channel += c;
+    }
+  }
+
+  Console.WriteLine("Receive channels: " + string.Join(", ", foundChannels));
+}
+
+ParseNotificationChannel("[BE][FE][Urgent] there is error");
+ParseNotificationChannel("[BE][QA][HAHA][Urgent] there is error");
